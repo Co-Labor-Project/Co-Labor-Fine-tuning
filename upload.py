8d9987f8afdc9456df85dc7f8862405692eff2cd
mk-isos <mkisos7900@naver.com>
2025-02-25 10:53:48 +0900

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,9 +1,18 @@
+import os
 from openai import OpenAI
 
 client = OpenAI(api_key="")
 
+# response = client.files.create(
+#     file=open("train-data-test.jsonl", "rb"), purpose="fine-tune"
+# )
+
+# 현재 `upload.py`가 있는 폴더 기준으로 파일 경로 설정
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "data.jsonl")
+
 response = client.files.create(
-    file=open("train-data-test.jsonl", "rb"), purpose="fine-tune"
+    file=open(file_path, "rb"), purpose="fine-tune"
 )
 
 print(response)
